ae27770c5e30076c2d914770cfb98cee99296832
Jayut1 <jshakhaha@gmail.com>
2024-12-09 14:36:26 +0545

    first commit

---
+++ custom.js
@@ -0,0 +1,13 @@
+var name="jayut"
+var email="jayut@gmail.com"
+var rol_name="1"
+var address="Lagan"
+var phone="980356798"
+//var age =30
+//console.log("My name is "+name + " age " +age)
+//console.log(`my name is ${name} age ${age}` )
+document.getElementById("name").innerHTML=name
+document.getElementById("email").innerHTML=email
+document.getElementById("rol_name").innerHTML=rol_name
+document.getElementById("address").innerHTML=address
+document.getElementById("phone").innerHTML=phone
---
+++ user.js
@@ -0,0 +1,6 @@
+var name="Jayut"
+var password="12345"
+var email="jayut@gmail.com"
+var age =30
+//console.log("My name is "+name + " age " +age)
+console.log(`my name is ${name} age ${age} password ${password} email ${email}` )
